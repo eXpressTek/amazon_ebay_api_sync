8efb1c8b40b6bd7a5dfef89ab8d11eb3826ce7d6
root <root@xtdfrlx06.(none)>
2014-05-10 17:06:18 -0600

    updates for python 2.7

--- ebay_search.py
+++ ebay_search.py
@@ -97,18 +97,24 @@ for search_tuple in search_return:
         print "{1} got_sku={0}".format(sku,stamp()),
         
         # check if its already in the database
-        recordNum = cursor.execute("SELECT * FROM sync WHERE ItemID LIKE \"%s\"", (sku))
+        recordNum = cursor.execute("SELECT * FROM sync WHERE ItemID LIKE \"{0}\"".format(sku))
+
+	# initialize the sql_statement
+	sql_statement = ''
 
         # if its not in the database, INSERT the new record
         if (recordNum == 0L) or (recordNum == 0):
         
-            print "sku doesnt exist INSERT'ing new entry"
-            cursor.execute("INSERT INTO sync (ItemID, Type, Images, LastUpdate, SubCategory, Category, Price, CurrencyID, Description, Title, Seller, URL) VALUES (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", %s, \"%s\", \"%s\", \"%s\", \"%s\", \"%s\")",(sku, poller_type, images, lastUpdate, subcategory, category, price, currency, description, title, seller, url))
+            print "sku does NOT exist INSERT'ing new entry"
+            sql_statement = "INSERT INTO sync (ItemID, Type, Images, LastUpdate, SubCategory, Category, Price, CurrencyID, Description, Title, Seller, URL) VALUES (\"{0}\", \"{1}\", \"{2}\", \"{3}\", \"{4}\", \"{5}\", {6}, \"{7}\", \"{8}\", \"{9}\", \"{10}\", \"{11}\")".format(sku, poller_type, images, lastUpdate, subcategory, category, price, currency, description, title, seller, url)
 
         # else its an existing record and we need to update
         else:
             print "sku does exist UPDATE'ing the EXISTING entry"
-            cursor.execute("UPDATE sync SET ItemID=\"%s\", Type=\"%s\", Images=\"%s\", LastUpdate=\"%s\", SubCategory=\"%s\", Category=\"%s\", Price=%s, CurrencyID=\"%s\", Description=\"%s\", Title=\"%s\", Seller=\"%s\", URL=\"%s\" WHERE ItemID=%s",(sku, poller_type, images, lastUpdate, subcategory, category, price, currency, description, title, seller, url, sku))
+            sql_statement = "UPDATE sync SET ItemID=\"{0}\", Type=\"{1}\", Images=\"{2}\", LastUpdate=\"{3}\", SubCategory=\"{4}\", Category=\"{5}\", Price={6}, CurrencyID=\"{7}\", Description=\"{8}\", Title=\"{9}\", Seller=\"{10}\", URL=\"{11}\" WHERE ItemID={12}".format(sku, poller_type, images, lastUpdate, subcategory, category, price, currency, description, title, seller, url, sku)
+
+        print "{0} DEBUG: {1}".format(stamp(),sql_statement)
+        cursor.execute(sql_statement)
 
         
 print "{0} ebay_sync.py complete".format(stamp())
