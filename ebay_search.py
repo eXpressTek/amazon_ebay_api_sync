084177514d54655df6338f4d2760a1abca3d2215
Warren Kenner <warren@thekenners.com>
2014-05-10 17:07:57 -0600

    updated unicode escaping

--- ebay_search.py
+++ ebay_search.py
@@ -30,7 +30,7 @@ cursor = db.cursor()
 ######################################
 # grab the ebay credentials from the config table of the database
 ######################################
-cursor.execute("SELECT * FROM sync_config WHERE Sync_Type LIKE \"ebay\"")
+cursor.execute("SELECT * FROM sync_config WHERE Sync_Type LIKE 'ebay'")
 
 #get the return values
 dbreturn = cursor.fetchall()
@@ -45,7 +45,7 @@ keydict = json.loads(synckey)
 ######################################
 # grab the searches from the database
 ######################################
-cursor.execute("SELECT * FROM sync_searches WHERE search_poller LIKE \"ebay\"")
+cursor.execute("SELECT * FROM sync_searches WHERE search_poller LIKE 'ebay'")
 
 #get the return values
 search_return = cursor.fetchall()
@@ -97,7 +97,7 @@ for search_tuple in search_return:
         print "{1} got_sku={0}".format(sku,stamp()),
         
         # check if its already in the database
-        recordNum = cursor.execute("SELECT * FROM sync WHERE ItemID LIKE \"{0}\"".format(sku))
+        recordNum = cursor.execute("SELECT * FROM sync WHERE ItemID LIKE '{0}'".format(sku))
 
 	# initialize the sql_statement
 	sql_statement = ''
@@ -106,12 +106,12 @@ for search_tuple in search_return:
         if (recordNum == 0L) or (recordNum == 0):
         
             print "sku does NOT exist INSERT'ing new entry"
-            sql_statement = "INSERT INTO sync (ItemID, Type, Images, LastUpdate, SubCategory, Category, Price, CurrencyID, Description, Title, Seller, URL) VALUES (\"{0}\", \"{1}\", \"{2}\", \"{3}\", \"{4}\", \"{5}\", {6}, \"{7}\", \"{8}\", \"{9}\", \"{10}\", \"{11}\")".format(sku, poller_type, images, lastUpdate, subcategory, category, price, currency, description, title, seller, url)
+            sql_statement = u"""INSERT INTO sync (ItemID, Type, Images, LastUpdate, SubCategory, Category, Price, CurrencyID, Description, Title, Seller, URL) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', {6}, '{7}', '{8}', '{9}', '{10}', '{11}')""".format(sku, poller_type, images, lastUpdate, subcategory, category, price, currency, description, title, seller, url)
 
         # else its an existing record and we need to update
         else:
             print "sku does exist UPDATE'ing the EXISTING entry"
-            sql_statement = "UPDATE sync SET ItemID=\"{0}\", Type=\"{1}\", Images=\"{2}\", LastUpdate=\"{3}\", SubCategory=\"{4}\", Category=\"{5}\", Price={6}, CurrencyID=\"{7}\", Description=\"{8}\", Title=\"{9}\", Seller=\"{10}\", URL=\"{11}\" WHERE ItemID={12}".format(sku, poller_type, images, lastUpdate, subcategory, category, price, currency, description, title, seller, url, sku)
+            sql_statement = u"""UPDATE sync SET ItemID='{0}', Type='{1}', Images='{2}', LastUpdate='{3}', SubCategory='{4}', Category='{5}', Price={6}, CurrencyID='{7}', Description='{8}', Title='{9}', Seller='{10}', URL='{11}' WHERE ItemID={12}""".format(sku, poller_type, images, lastUpdate, subcategory, category, price, currency, description, title, seller, url, sku)
 
         print "{0} DEBUG: {1}".format(stamp(),sql_statement)
         cursor.execute(sql_statement)
