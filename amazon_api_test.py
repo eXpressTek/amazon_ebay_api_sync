824afa4aef259ada8b5a954692afa8b13919e29c
root <root@xtddmlx01.(none)>
2014-05-18 07:37:52 -0600

    got ebay working but needs reveiw and amazon 90% done (pagination should be the last thing to be done)

--- amazon_api_test.py
+++ amazon_api_test.py
@@ -86,7 +86,7 @@ if search_return is 0L:
 for search_tuple in search_return:
 
     # unpack search tuple to get search text
-    search_num, search_poller, search_text = search_tuple
+    search_num, search_poller, search_text, number_of_results = search_tuple
     
     # encode search text as string
     if debug:
@@ -119,36 +119,68 @@ for search_tuple in search_return:
             print dir(itemXMLdict)
             print itemXMLdict.keys()
         itemInfo = itemXMLdict['ItemLookupResponse']['Items']['Item']
-        print json.dumps(itemInfo)
-        sys.exit(0)
         
         #mappings for the database fields
-        sku = itemInfo['ASIN']
-        poller_type = "amazon"
-        images = itemInfo['MediumImage']['URL']
-        lastUpdate = time.time()
-        category = itemInfo['ItemAttributes']['ProductGroup']
-        dirty_subcategory = itemInfo['BrowseNodes']['BrowseNode']
-        subcategory = ""
-        for sub in dirty_subcategory:
-            subcategory = subcategory + sub['Name'] + " "
-        subcategory = str(subcategory)
-        price = float(itemInfo['ItemAttributes']['ListPrice']['Amount'])/100
-        currency = itemInfo['ItemAttributes']['ListPrice']['CurrencyCode']
-        dirty_description = itemInfo['ItemAttributes']['Feature']
-        description = ""
-        #sanitize the description
-        for desc in dirty_description:
-            if debug:
-                print str(desc).decode('ascii', 'ignore')
-            desc = re.sub('\\\\', '', desc)
-            description = description + desc
-        title = itemInfo['ItemAttributes']['Title']
-        print title
-        seller = itemInfo['ItemAttributes']['Publisher']
-        url = itemInfo['DetailPageURL']
-        manufacturer = itemInfo['ItemAttributes']['Manufacturer']
-        brand = itemInfo['ItemAttributes']['Brand']
+        try:
+            sku = itemInfo['ASIN']
+            poller_type = "amazon"
+            images = itemInfo['MediumImage']['URL']
+            lastUpdate = time.time()
+            category = itemInfo['ItemAttributes']['ProductGroup']
+            dirty_subcategory = itemInfo['BrowseNodes']['BrowseNode']
+            subcategory = ""
+            for sub in dirty_subcategory:
+                try:
+                    subcategory = subcategory + sub['Name'] + " "
+                except TypeError as e:
+                    try:
+                        for sub_sub in sub:
+                            subcategory = subcategory + sub_sub['Name']+" "
+                    except TypeError as f:
+                        subcategory = "None"
+            subcategory = str(subcategory)
+            price = {}
+            if 'ListPrice' in itemInfo['ItemAttributes'].keys():
+                price['ListPrice'] = {}
+                price['ListPrice']['Amount'] = float(itemInfo['ItemAttributes']['ListPrice']['Amount'])/100
+                price['ListPrice']['Currency'] = itemInfo['ItemAttributes']['ListPrice']['CurrencyCode']
+            if 'OfferSummary' in itemInfo.keys():
+                if 'LowestUsedPrice' in itemInfo['OfferSummary'].keys():
+                    price['LowestUsedPrice'] = {}
+                    price['LowestUsedPrice']['Amount'] = float(itemInfo['OfferSummary']['LowestUsedPrice']['Amount'])/100
+                    price['LowestUsedPrice']['Currency'] = itemInfo['OfferSummary']['LowestUsedPrice']['CurrencyCode']
+                if 'LowestNewPrice' in itemInfo['OfferSummary'].keys():
+                    price['LowestNewPrice'] = {}
+                    price['LowestNewPrice']['Amount'] = float(itemInfo['OfferSummary']['LowestNewPrice']['Amount'])/100
+                    price['LowestNewPrice']['Currency'] = itemInfo['OfferSummary']['LowestNewPrice']['CurrencyCode']
+            description = ""
+            if 'Feature' in itemInfo['ItemAttributes'].keys():
+                dirty_description = itemInfo['ItemAttributes']['Feature']
+                #sanitize the description
+                for desc in dirty_description:
+                    if debug:
+                        print str(desc).decode('ascii', 'ignore')
+                    desc = re.sub('\\\\', '', desc)
+                    description = description + desc
+            else:
+                description = "None"
+            title = itemInfo['ItemAttributes']['Title']
+            seller = ""
+            if 'Publisher' in itemInfo['ItemAttributes'].keys():
+                seller = itemInfo['ItemAttributes']['Publisher']
+            url = itemInfo['DetailPageURL']
+            manufacturer = ""
+            if 'Manufacturer' in itemInfo['ItemAttributes'].keys():
+                manufacturer = itemInfo['ItemAttributes']['Manufacturer']
+            brand = ""
+            if 'Brand' in itemInfo['ItemAttributes'].keys():
+                brand = itemInfo['ItemAttributes']['Brand']
+        except KeyError as err:
+            #if debug:
+            print err.message
+            print json.dumps(itemInfo)
+            print stamp()+" got malformed data from amazon, skipping."
+            continue
         
         if debug:
             print ""
@@ -173,55 +205,60 @@ for search_tuple in search_return:
         
         #print progress of SKU's with timestamp
         print "{1} got_sku={0}".format(sku,stamp()),
-        
-        # check if its already in the database
-        recordNum = cursor.execute("SELECT * FROM sync WHERE ItemID LIKE '{0}'".format(sku))
-
-        # initialize the sql_statement
-        sql_statement = ''
-
-        # if its not in the database, INSERT the new record
-        if (recordNum == 0L) or (recordNum == 0):
-        
-            print "sku does NOT exist INSERT'ing new entry"
-            sql_statement = u"""INSERT INTO sync (ItemID, Type, Images, Category, Price, CurrencyID, Description, Title, Seller, subcategory, URL, Brand, Manufacturer, LastUpdate) VALUES ('{0}', '{1}', '{2}', '{3}', {4}, '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}', {13})""".format(
-             sku.encode('utf-8'),
-             db.escape_string(poller_type.encode('utf-8')),
-             db.escape_string(images.encode('utf-8')),
-             db.escape_string(category.encode('utf-8')),
-             price,
-             db.escape_string(currency.encode('utf-8')),
-             db.escape_string(json.dumps(description).encode('utf-8')),
-             db.escape_string(title.encode('utf-8')),
-             db.escape_string(seller).encode('utf-8'),
-             db.escape_string(subcategory.encode('utf-8')),
-             db.escape_string(url.encode('utf-8')),
-             lastUpdate
-            )
-
-        # else its an existing record and we need to update
-        else:
-            print "sku does exist UPDATE'ing the EXISTING entry"
-            sql_statement = u"""UPDATE sync SET Type='{0}', Images='{1}', LastUpdate='{2}', Category='{3}', Price={4}, CurrencyID='{5}', Description='{6}', Title='{7}', Seller='{8}', URL='{9}', subcategory='{10}' WHERE ItemID='{11}'""".format(
-             db.escape_string(poller_type),
-             db.escape_string(images),
-             lastUpdate,
-             db.escape_string(category),
-             price,
-             db.escape_string(currency),
-             db.escape_string(str(json.dumps(description).decode('ascii', 'ignore'))),
-             db.escape_string(str(title.decode('ascii', 'ignore'))),
-             db.escape_string(seller),
-             db.escape_string(url),
-             db.escape_string(json.dumps(subcategory)),
-             sku
-            )
-
-#        print "{0} DEBUG: {1}".format(stamp(),sql_statement)
-        cursor.execute(sql_statement)
-
-        # make it sleep a tick before starting the next record to allow for rate-limiting behavior
-        time.sleep(.3)
+        try:
+            # check if its already in the database
+            recordNum = cursor.execute("SELECT * FROM sync_amazon WHERE ItemID LIKE '{0}'".format(sku))
+            
+            # initialize the sql_statement
+            sql_statement = ''
+            
+            # if its not in the database, INSERT the new record
+            if (recordNum == 0L) or (recordNum == 0):
+            
+                print "sku does NOT exist INSERT'ing new entry"
+                sql_statement = u"""INSERT INTO sync_amazon (ItemID, Type, Images, Category, Price, Description, Title, Seller, subcategory, URL, Brand, Manufacturer, LastUpdate) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}')""".format(
+                 db.escape_string(sku.encode('utf-8')),
+                 db.escape_string(poller_type.encode('utf-8')),
+                 db.escape_string(images.encode('utf-8')),
+                 db.escape_string(category.encode('utf-8')),
+                 db.escape_string(json.dumps(price).encode('utf-8')),
+                 db.escape_string(json.dumps(description).encode('utf-8')),
+                 db.escape_string(title.encode('utf-8')),
+                 db.escape_string(seller).encode('utf-8'),
+                 db.escape_string(subcategory.encode('utf-8')),
+                 db.escape_string(url.encode('utf-8')),
+                 db.escape_string(brand.encode('utf-8')),
+                 db.escape_string(manufacturer.encode('utf-8')),
+                 float(lastUpdate)
+                )
+            
+            # else its an existing record and we need to update
+            else:
+                print "sku does exist UPDATE'ing the EXISTING entry"
+                sql_statement = u"""UPDATE sync_amazon SET Type='{0}', Images='{1}', LastUpdate='{2}', Category='{3}', Price='{4}', Description='{5}', Title='{6}', Seller='{7}', URL='{8}', subcategory='{9}' WHERE ItemID='{10}'""".format(
+                 db.escape_string(poller_type),
+                 db.escape_string(images),
+                 float(lastUpdate),
+                 db.escape_string(category),
+                 db.escape_string(json.dumps(price).encode('utf-8')),
+                 db.escape_string(str(json.dumps(description).decode('ascii', 'ignore'))),
+                 db.escape_string(str(title.decode('ascii', 'ignore'))),
+                 db.escape_string(seller),
+                 db.escape_string(url),
+                 db.escape_string(json.dumps(subcategory)),
+                 db.escape_string(sku)
+                )
+            
+#            print "{0} DEBUG: {1}".format(stamp(),sql_statement)
+            cursor.execute(sql_statement)
+            
+            # make it sleep a tick before starting the next record to allow for rate-limiting behavior
+            time.sleep(.3)
+        except UnicodeDecodeError as u:
+            if debug:
+                print u
+            print stamp()+" Malformed Data - Unicode Decode Error - Skipping"
+            continue
 
 print "{0} sync_amazon.py complete".format(stamp())
     
--- sync_ebay.py
+++ sync_ebay.py
@@ -7,9 +7,6 @@
 # 2014.05.10 eXpressTek Inc, initial release (Warren Kenner)
 #
 
-class ConnectionError(Exception):
-    pass
-
 from ebaysdk.finding import Connection as finding
 from ebaysdk.shopping import Connection as shopping
 import json
@@ -20,20 +17,32 @@ import sys
 import inspect
 import subprocess
 
-debug = False
+#make a timestamp
+def stamp():
+  return time.strftime("%a, %d %b %Y %H:%M:%S +0000 ",time.gmtime())
 
+debug = False
+timing = True
+smallDescription = True
+
+#this is to get the connections to be quiet.
+class ConnectionError(Exception):
+    pass
+
+if timing:
+    print stamp()+"doing a version check"
 #do a version check
 version_check = subprocess.check_output(['git', 'diff', 'origin/master'])
 if (version_check.strip() != ""):
     print "WARN: This software may be out of date. Please see about updating"
 
-#make a timestamp
-def stamp():
-  return time.strftime("%a, %d %b %Y %H:%M:%S +0000",time.gmtime())
-
+if timing:
+    print stamp()+"getting creds from files and database"
 #get the credentials for the database from the db.conf file. (the import credentials is the actual function to do this.)
 creds = credentials.load_file_config()
 
+if timing:
+    print stamp()+"connecting to the database"
 #connect to the database
 db = MySQLdb.connect(host=creds['host'], port=int(creds['port']), user=creds['user'], passwd=creds['password'], db=creds['database'])
 
@@ -54,7 +63,8 @@ synctype, syncvalue, synckey = key_tuple
 
 #reconstitute the json to a dictionary 
 keydict = json.loads(synckey)
-
+if timing:
+    print stamp()+"got creds - Finding Searches"
 ######################################
 # grab the searches from the database
 ######################################
@@ -68,142 +78,171 @@ if search_return is 0L:
 
 #run for each search returned
 for search_tuple in search_return:
-    
+    if timing:
+        print stamp()+"running searches"
     # unpack search tuple to get search text
-    search_num, search_poller, search_text = search_tuple
+    search_num, search_poller, search_text, number_of_results = search_tuple
     
-    # encode search text as string
-    search_text = search_text.encode('ascii','ignore')
-    try:
-        # establish connection using key obtained from database
-        find = finding(appid=keydict['key'])
-        
-        #execute the search once connected. er
-        find.execute('findItemsAdvanced', {'keywords': search_text, 'paginationInput':{'entriesPerPage':100, 'pageNumber':1}, 'itemFilter':{'name':'ListingType','value':'AuctionWithBIN'}, 'itemFilter':{'name':'ListingType','value':'FixedPrice'}})
+    results = 0
+    while (results <= number_of_results):
+        # encode search text as string
+        search_text = search_text.encode('ascii','ignore')
+        try:
+            # establish connection using key obtained from database
+            find = finding(appid=keydict['key'])
+            if timing:
+                print stamp()+"about to make api call"
+            #execute the search once connected. er
+            page = int(results/100)
+            find.execute('findItemsAdvanced', {'keywords': search_text, 'paginationInput':{'entriesPerPage':100, 'pageNumber':page}, 'itemFilter':{'name':'ListingType','value':'AuctionWithBIN'}, 'itemFilter':{'name':'ListingType','value':'FixedPrice'}})
+            if timing:
+                print stamp()+"finished making api call (executed)"
+        except ConnectionError as e:
+            print "got a Connection Error"
+            raise e
+            sys.exit(0)
+        #get the response dictionary from the call
+        mydict = find.response_dict()
+        if debug:
+            print "mydict: "
+            print mydict
     
-    except ConnectionError as e:
-        print "got a Connection Error"
-        raise e
-        sys.exit(0)
-    #get the response dictionary from the call
-    mydict = find.response_dict()
-    if debug:
-        print "mydict: "
-        print mydict
-
-    #unpack the return items and map to relevant Database fields
-    items = mydict['searchResult']['item']
-    item_ids = []
-    for item in items:
-        item_ids.append(item['itemId']['value'])
-    if debug:
-        print "Item IDs: "
-        print item_ids
-    pos = 0
-    listNum = 0
-    separated_item_ids = []
-    separated_item_ids.append([])
-    for item_id in item_ids:
-        if (pos % 20 == 0) and (pos != 0):
-            listNum = listNum + 1
-            separated_item_ids.append([])
-        separated_item_ids[listNum].append(item_id)
-        pos = pos + 1
-    specifics = {}
-    try: 
-        for item_id_list in separated_item_ids:
-            shop = shopping(appid=keydict['key'])
-            shop.execute('GetMultipleItems', {'itemID':item_id_list, 'includeSelector':'Details, ItemSpecifics, Description'})
-            if debug:
-                print dir(shop)
-            specifics_partial_dict = shop.response_dict()
-            if specifics_partial_dict is None:
-                print "Unable to get specifics for some reason, exiting"
-                sys.exit(0)
-            if debug:
-                print specifics_partial_dict
-            specifics_dict = specifics_partial_dict['Item']
-            for item in specifics_dict:
-                specifics[item['ItemID']['value']] = item
-    except ConnectionError as e:
-        print "got a Connection Error"
-        raise e
-        sys.exit(0)
-    for item in items:
-        sku = item['itemId']['value']
-        poller_type = "ebay"
-        seller = ''
-        images = item['galleryURL']['value']
-        lastUpdate = time.time()
-        category = item['primaryCategory']['categoryName']['value']
-        price = item['sellingStatus']['currentPrice']['value']
-        currency = item['sellingStatus']['currentPrice']['currencyId']['value']
-        title = item['title']['value']
-        url = item['viewItemURL']['value']
-        specifics_object = specifics[sku]
-        if "ItemSpecifics" in specifics_object.keys():
-            item_specifics_list = specifics_object['ItemSpecifics']['NameValueList']
-            item_specifics = ""
-            for item_specs in item_specifics_list:
-                try:
-                    item_specifics = item_specifics + item_specs['Name']['value']+" : "+item_specs['Value']['value']+", "
-                except TypeError as e:
-                    item_specifics = item_specs
-            if isinstance(item_specifics, basestring):
-                item_specifics = item_specifics[:-2]
-            item['specifics'] = specifics_object
-        description = ''
-        if "Description" in specifics_object.keys():
-            description = specifics_object["Description"]
-        
-        #print progress of SKU's with timestamp
-        print "{1} got_sku={0}".format(sku,stamp()),
-        
-        # check if its already in the database
-        recordNum = cursor.execute("SELECT * FROM sync_ebay WHERE ItemID LIKE '{0}'".format(sku))
-
-        # initialize the sql_statement
-        sql_statement = ''
-
-        # if its not in the database, INSERT the new record
-        if (recordNum == 0L) or (recordNum == 0):
-        
-            print "sku does NOT exist INSERT'ing new entry"
-            sql_statement = u"""INSERT INTO sync_ebay (ItemID, Type, Images, LastUpdate, Category, Price, CurrencyID, Description, Title, Seller, URL, ItemSpecifics) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', {5}, '{6}', '{7}', '{8}', '{9}', '{10}','{11}')""".format(
-             sku,
-             db.escape_string(poller_type),
-             db.escape_string(images),
-             lastUpdate,
-             db.escape_string(category),
-             price,
-             db.escape_string(currency),
-             db.escape_string(str(description)),
-             db.escape_string(title),
-             db.escape_string(seller),
-             db.escape_string(url),
-             db.escape_string(str(item_specifics))
-            )
-
-        # else its an existing record and we need to update
-        else:
-            print "sku does exist UPDATE'ing the EXISTING entry"
-            sql_statement = u"""UPDATE sync_ebay SET Type='{0}', Images='{1}', LastUpdate='{2}', Category='{3}', Price={4}, CurrencyID='{5}', Description='{6}', Title='{7}', Seller='{8}', URL='{9}', ItemSpecifics='{10}' WHERE ItemID={11}""".format(
-             db.escape_string(poller_type),
-             db.escape_string(images),
-             lastUpdate,
-             db.escape_string(category),
-             price,
-             db.escape_string(currency),
-             db.escape_string(str(description)),
-             db.escape_string(title),
-             db.escape_string(seller),
-             db.escape_string(url),
-             db.escape_string(str(item_specifics)),
-             sku
-            )
-
-#        print "{0} DEBUG: {1}".format(stamp(),sql_statement)
-        cursor.execute(sql_statement)
-
-        
+        #unpack the return items and map to relevant Database fields
+        if timing:
+            print stamp()+"getting each item"
+        items = mydict['searchResult']['item']
+        item_ids = []
+        if timing:
+            print stamp()+"packing into a big list"
+        for item in items:
+            item_ids.append(item['itemId']['value'])
+        if debug:
+            print "Item IDs: "
+            print item_ids
+        if timing:
+            print stamp()+"finished packing into big list"
+        pos = 0
+        listNum = 0
+        separated_item_ids = []
+        separated_item_ids.append([])
+        if timing:
+            print stamp()+"separating big list into smaller list for api call"
+        for item_id in item_ids:
+            if (pos % 20 == 0) and (pos != 0):
+                listNum = listNum + 1
+                separated_item_ids.append([])
+            separated_item_ids[listNum].append(item_id)
+            pos = pos + 1
+        specifics = {}
+        if timing:
+            print stamp()+"finished separating into smaller list, asking ebay for additional information"
+        try: 
+            for item_id_list in separated_item_ids:
+                shop = shopping(appid=keydict['key'])
+                if timing:
+                    print stamp()+"executing api getMultipleItems call"
+                shop.execute('GetMultipleItems', {'itemID':item_id_list, 'includeSelector':'Details, ItemSpecifics, Description'})
+                if timing:
+                    print stamp()+"finished getting previous call"
+                if debug:
+                    print dir(shop)
+                specifics_partial_dict = shop.response_dict()
+                if specifics_partial_dict is None:
+                    print "Unable to get specifics for some reason, exiting"
+                    sys.exit(0)
+                if debug:
+                    print specifics_partial_dict
+                specifics_dict = specifics_partial_dict['Item']
+                if timing:
+                    print stamp()+"putting current data into overarching dict"
+                for item in specifics_dict:
+                    specifics[item['ItemID']['value']] = item
+                if timing:
+                    print stamp()+"finished putting current data into data"
+        except ConnectionError as e:
+            print "got a Connection Error"
+            raise e
+            sys.exit(0)
+        if timing:
+            print stamp()+"finished asking for additional information, separating into each one, and mapping to database"
+        for item in items:
+            sku = item['itemId']['value']
+            poller_type = "ebay"
+            seller = ''
+            images = item['galleryURL']['value']
+            lastUpdate = time.time()
+            category = item['primaryCategory']['categoryName']['value']
+            price = item['sellingStatus']['currentPrice']['value']
+            currency = item['sellingStatus']['currentPrice']['currencyId']['value']
+            title = item['title']['value']
+            url = item['viewItemURL']['value']
+            specifics_object = specifics[sku]
+            if "ItemSpecifics" in specifics_object.keys():
+                item_specifics_list = specifics_object['ItemSpecifics']['NameValueList']
+                item_specifics = ""
+                for item_specs in item_specifics_list:
+                    try:
+                        item_specifics = item_specifics + item_specs['Name']['value']+" : "+item_specs['Value']['value']+", "
+                    except TypeError as e:
+                        item_specifics = item_specs
+                if isinstance(item_specifics, basestring):
+                    item_specifics = item_specifics[:-2]
+                item['specifics'] = specifics_object
+            description = ''
+            if "Description" in specifics_object.keys():
+                description = specifics_object["Description"]
+            if smallDescription:
+                description = "TEST"
+            
+            #print progress of SKU's with timestamp
+            print "{1} got_sku={0}".format(sku,stamp()),
+            
+            # check if its already in the database
+            recordNum = cursor.execute("SELECT * FROM sync_ebay WHERE ItemID LIKE '{0}'".format(sku))
+    
+            # initialize the sql_statement
+            sql_statement = ''
+    
+            # if its not in the database, INSERT the new record
+            if (recordNum == 0L) or (recordNum == 0):
+            
+                print "sku does NOT exist INSERT'ing new entry"
+                sql_statement = u"""INSERT INTO sync_ebay (ItemID, Type, Images, LastUpdate, Category, Price, CurrencyID, Description, Title, Seller, URL, ItemSpecifics) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', {5}, '{6}', '{7}', '{8}', '{9}', '{10}','{11}')""".format(
+                 sku,
+                 db.escape_string(poller_type),
+                 db.escape_string(images),
+                 lastUpdate,
+                 db.escape_string(category),
+                 price,
+                 db.escape_string(currency),
+                 db.escape_string(str(description)),
+                 db.escape_string(title),
+                 db.escape_string(seller),
+                 db.escape_string(url),
+                 db.escape_string(str(item_specifics))
+                )
+    
+            # else its an existing record and we need to update
+            else:
+                print "sku does exist UPDATE'ing the EXISTING entry"
+                sql_statement = u"""UPDATE sync_ebay SET Type='{0}', Images='{1}', LastUpdate='{2}', Category='{3}', Price={4}, CurrencyID='{5}', Description='{6}', Title='{7}', Seller='{8}', URL='{9}', ItemSpecifics='{10}' WHERE ItemID={11}""".format(
+                 db.escape_string(poller_type),
+                 db.escape_string(images),
+                 lastUpdate,
+                 db.escape_string(category),
+                 price,
+                 db.escape_string(currency),
+                 db.escape_string(str(description)),
+                 db.escape_string(title),
+                 db.escape_string(seller),
+                 db.escape_string(url),
+                 db.escape_string(str(item_specifics)),
+                 sku
+                )
+    
+    #        print "{0} DEBUG: {1}".format(stamp(),sql_statement)
+            cursor.execute(sql_statement)
+            results = results + 1
+    
+            
 print "{0} sync_ebay.py complete".format(stamp())
